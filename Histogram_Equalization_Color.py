0e68d8e4d709fe65e0afacac0dc00e73309508ff
abubakr-shafique <54443371+abubakr-shafique@users.noreply.github.com>
2019-09-19 19:51:44 +0800

    Fix Your Low Contrast Image

--- Histogram_Equalization_Color.py
+++ Histogram_Equalization_Color.py
@@ -1,71 +1,66 @@
-##This Program is written by Abubakr Shafique (abubakr.shafique@gmail.com)
-import numpy as np
-import cv2 as cv
+# This program is written by Abubakr Shafique (abubakr.shafique@gmail.com) 
+import numpy as np	#This is to deal with numbers and arrays
+import cv2 as cv	#This is to deal with images
 
 def Max(Current_Value, New_Value):
 	if New_Value > Current_Value:
 		return New_Value
 	else:
 		return Current_Value
-
+		
 def Min(Current_Value, New_Value):
 	if New_Value < Current_Value:
 		return New_Value
 	else:
 		return Current_Value
-		
-#This Histogram Function Takes in a Mat Image and Calculate Histogram
-def Calculate_Histogram_Color(Image):
-	Image_Height = Image.shape[1] #get the Height of the Image
-	Image_Width = Image.shape[0] #get the Width of the Image
-	Image_Channels = Image.shape[2] #get the Channels of the Image
+
+def Histogram_Computation(Image):
+	Image_Height = Image.shape[0]
+	Image_Width = Image.shape[1]
+	Image_Channels = Image.shape[2]
+	
+	Histogram = np.zeros([256, Image_Channels], np.int32)
 	
 	Max_Intensity = np.zeros([Image_Channels], np.int32)
 	Min_Intensity = np.full([Image_Channels], 255, np.int32)
 	
-	Hist = np.zeros([256,Image_Channels], np.int32) #Initialize an empty 256 array for each channel
-	
-	for x in range(0,Image_Width): # loop through the Image
-		for y in range(0,Image_Height):
+	for x in range(0, Image_Height):
+		for y in range(0, Image_Width):
 			for c in range(0, Image_Channels):
-				Hist[Image[x,y,c],c] +=1 # increment the same intensity values
+				Histogram[Image[x,y,c],c] +=1
 				Max_Intensity[c] = Max(Max_Intensity[c], Image[x,y,c])
 				Min_Intensity[c] = Min(Min_Intensity[c], Image[x,y,c])
-			
-	return Hist, Max_Intensity, Min_Intensity
+	
+	return Histogram, Min_Intensity, Max_Intensity
 
-def New_Pixel_Value(Previous_Value, Max, Min):
+def New_Pixel_Value(Current_Value, Min, Max):
 	Target_Max = 255
 	Target_Min = 0
-	return (Target_Min + (Previous_Value - Min) * int((Target_Max - Target_Min)/(Max-Min)))
+	return (Target_Min + (Current_Value - Min) * int(Target_Max-Target_Min)/(Max-Min))
 
-def Histogram_Equalization(Image, Max, Min):
-	Image_Height = Image.shape[1] #get the Height of the Image
-	Image_Width = Image.shape[0] #get the Width of the Image
-	Image_Channels = Image.shape[2] #get the Channels of the Image
+def Histogram_Equalization(Image, Min, Max):
+	Image_Height = Image.shape[0]
+	Image_Width = Image.shape[1]
+	Image_Channels = Image.shape[2]
+	Size = (Image_Height, Image_Width, Image_Channels)
 	
-	Size = (Image_Width, Image_Height, Image_Channels)
 	New_Image = np.zeros(Size, np.uint8)
 	
-	for x in range(0,Image_Width): # loop through the Image
-		for y in range(0,Image_Height):
+	for x in range(0, Image_Height):
+		for y in range(0, Image_Width):
 			for c in range(0, Image_Channels):
-				New_Image[x,y,c] = New_Pixel_Value(Image[x,y,c], Max[c], Min[c])
+				New_Image[x,y,c] = New_Pixel_Value(Image[x,y,c], Min[c], Max[c])
 	
 	return New_Image
-	
 def main():
-	Input_Image = cv.imread('Low_Contrast_Color.jpg')
-	Histogram, Max_Intensity, Min_Intensity = Calculate_Histogram_Color(Input_Image)
+	Input_Image = cv.imread("Low_Contrast_Color.jpg") # This is to read the color Image
 	
-	New_Image = Histogram_Equalization(Input_Image, Max_Intensity, Min_Intensity)
+	Histogram_GrayScale, Min, Max = Histogram_Computation(Input_Image)
 	
-	#Now We Will show the result
-	cv.imshow("Fixed_Image", New_Image)
-	cv.waitKey()
+	New_Image = Histogram_Equalization(Input_Image, Min, Max)
+	#Now to print our output Histogram
+	cv.imwrite("Fixed_Low_Contrast.png", New_Image)
+	input("Please Enter to Continue...")
 	
-	input("Press Enter to continue...")
-
-
 if __name__ == '__main__':
-	main() #Our main function
+	main()
--- Histogram_Equalization_GrayScale.py
+++ Histogram_Equalization_GrayScale.py
@@ -1,66 +1,61 @@
-##This Program is written by Abubakr Shafique (abubakr.shafique@gmail.com)
-import numpy as np
-import cv2 as cv
+# This program is written by Abubakr Shafique (abubakr.shafique@gmail.com) 
+import numpy as np	#This is to deal with numbers and arrays
+import cv2 as cv	#This is to deal with images
 
 def Max(Current_Value, New_Value):
 	if New_Value > Current_Value:
 		return New_Value
 	else:
 		return Current_Value
-
+		
 def Min(Current_Value, New_Value):
 	if New_Value < Current_Value:
 		return New_Value
 	else:
 		return Current_Value
-		
-#This Histogram Function Takes in a Mat Image and Calculate Histogram
-def Calculate_Histogram_Gray(Image):
-	Image_Height = Image.shape[1] #get the Height of the Image
-	Image_Width = Image.shape[0] #get the Width of the Image
+
+def Histogram_Computation(Image):
+	Image_Height = Image.shape[0]
+	Image_Width = Image.shape[1]
+	Histogram = np.zeros([256], np.int32)
 	
 	Max_Intensity = 0
 	Min_Intensity = 255
 	
-	Hist = np.zeros([256], np.int32) #Initialize an empty 256 array
-	
-	for x in range(0,Image_Width): # loop through the Image
-		for y in range(0,Image_Height):
-			Hist[Image[x,y]] +=1 # increment the same intensity values
+	for x in range(0, Image_Height):
+		for y in range(0, Image_Width):
+			Histogram[Image[x,y]] +=1
 			Max_Intensity = Max(Max_Intensity, Image[x,y])
 			Min_Intensity = Min(Min_Intensity, Image[x,y])
-			
-	return Hist, Max_Intensity, Min_Intensity
+	
+	return Histogram, Min_Intensity, Max_Intensity
 
-def New_Pixel_Value(Previous_Value, Max, Min):
+def New_Pixel_Value(Current_Value, Min, Max):
 	Target_Max = 255
 	Target_Min = 0
-	return (Target_Min + (Previous_Value - Min) * int((Target_Max - Target_Min)/(Max-Min)))
+	return (Target_Min + (Current_Value - Min) * int(Target_Max-Target_Min)/(Max-Min))
 
-def Histogram_Equalization(Image, Max, Min):
-	Image_Height = Image.shape[1] #get the Height of the Image
-	Image_Width = Image.shape[0] #get the Width of the Image
-	Size = (Image_Width, Image_Height)
+def Histogram_Equalization(Image, Min, Max):
+	Image_Height = Image.shape[0]
+	Image_Width = Image.shape[1]
+	Size = (Image_Height, Image_Width)
+	
 	New_Image = np.zeros(Size, np.uint8)
 	
-	for x in range(0,Image_Width): # loop through the Image
-		for y in range(0,Image_Height):
-			New_Image[x,y] = New_Pixel_Value(Image[x,y], Max, Min)
+	for x in range(0, Image_Height):
+		for y in range(0, Image_Width):
+			New_Image[x,y] = New_Pixel_Value(Image[x,y], Min, Max)
 	
 	return New_Image
-	
 def main():
-	Input_Image = cv.imread('Low_Contrast.jpg',0) #This will read a GrayScale Image (Even if it is RGB it will read GrayScale)
-	Histogram_GrayScale, Max_Intensity, Min_Intensity = Calculate_Histogram_Gray(Input_Image)
+	Input_Image = cv.imread("Low_Contrast.jpg",0) # This is to read Gray Scale Image
 	
-	New_Image = Histogram_Equalization(Input_Image, Max_Intensity, Min_Intensity)
+	Histogram_GrayScale, Min, Max = Histogram_Computation(Input_Image)
 	
-	#Now We Will show the result
-	cv.imshow("Fixed_Image", New_Image)
-	cv.waitKey()
+	New_Image = Histogram_Equalization(Input_Image, Min, Max)
+	#Now to print our output Histogram
+	cv.imwrite("Fixed_Low_Contrast.png", New_Image)
+	input("Please Enter to Continue...")
 	
-	input("Press Enter to continue...")
-
-
 if __name__ == '__main__':
-	main() #Our main function
+	main()
